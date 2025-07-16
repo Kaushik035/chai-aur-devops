43b7a7823aeb31c39babe708d8dc381822e985a5
Kaushik035 <kaushikmandal629@gmail.com>
2025-07-17 00:22:23 +0530

    feat: configure Vite development server to listen on all interfaces

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100
+    },
+    host: '0.0.0.0',
+    port: 5173
+  },
   plugins: [react()],
 });
+
